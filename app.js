d257a3226d2e9052694cb7c661c9717d26c358d1
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-15 15:16:07 -0500

    Complete Q#3

--- app.js
+++ app.js
@@ -18,3 +18,16 @@ new Set([1, 1, 2, 2, 3, 4]);
 [...new Set("referee")].join("");
 // console.log([...new Set("referee")].join(""));
 // it returns "ref"
+
+// ******************************************************
+// Quick Question #3
+// ******************************************************
+
+// What does the Map m look like after running the following code?
+
+let m = new Map();
+m.set([1, 2, 3], true);
+m.set([1, 2, 3], false);
+// console.log(m);
+// m looks like this: { [1,2,3]: true, [1,2,3]: false } because arrays are reference types.
+// To avoid this "strange" behavior, we'd need to create a variable to hold each key that is an array.
