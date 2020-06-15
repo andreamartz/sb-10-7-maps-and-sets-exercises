a66661981c3556288c9b3f52b8627ec802eefa8a
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-15 15:06:11 -0500

    Complete Q#1

---
+++ app.js
@@ -0,0 +1,9 @@
+// Maps and Sets Exercise
+// ******************************************************
+// Quick Question #1
+// ******************************************************
+
+// What does the following code return?
+
+console.log(new Set([1, 1, 2, 2, 3, 4]));
+// it returns
